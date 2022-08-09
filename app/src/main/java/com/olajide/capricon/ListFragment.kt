e6007ece2b66413e5552cd936d7be66d3cd2d6ba
Bashorun Olajide John <bashorunolajide@gmail.com>
2022-08-09 14:47:15 +0100

    Initialized repository

---
+++ app/src/main/java/com/olajide/capricon/ListFragment.kt
@@ -0,0 +1,39 @@
+package com.olajide.capricon
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.olajide.capricon.databinding.FragmentListBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
